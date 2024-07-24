ef0d7ea7bb8bafa5a12009c8ec58093fba19b7ad
Vamshi Gugulothu <vamnaik021@gmail.com>
2024-07-24 18:47:16 +0530

    Create signup page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,9 +8,9 @@ export const signup = async (req,res,next) => {
     if (!username || 
         !email || 
         !password || 
-        username==='' 
-        || email==='' 
-        || password===''
+        username==='' || 
+        email==='' || 
+        password===''
     ) {
         next(errorHandler(400,'All fields are required'));
     }
