1988724e932901814f78634d9da40906ebe87183
Vamshi Gugulothu <vamnaik021@gmail.com>
2024-07-31 16:26:50 +0530

    Update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import {Link,useLocation} from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
     const path=useLocation().pathname
+    const {currentUser} = useSelector(state => state.user)
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm
@@ -59,11 +61,37 @@ export default function Header() {
             <Button className='w-12 h-10 hidden sm:inline  hover:bg-indigo-500 hover:text-white btn-custom' color='gray' pill>
                 <FaMoon />
             </Button>
+            {currentUser ? (
+              <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar 
+                alt='user'
+                img={currentUser.profilePicture} 
+                rounded
+                />
+              }
+              >
+                <Dropdown.Header>
+                    <span className='block text-sm'>@{currentUser.username}</span>
+                    <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                </Dropdown.Header>
+                <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+                <Dropdown.Item>Sign out</Dropdown.Item>
+              </Dropdown>
+            ):(
             <Link to='/sign-in'>
-            <Button className='bg-gradient-to-r from-purple-500 to-blue-500 text-white hover:bg-indigo-500 hover:text-white btn-custom'>
+            <Button gradientDuoTone={'purpleToBlue'} outline>
                 Sign In
             </Button>
             </Link>
+            )
+            } 
+            
             <Navbar.Toggle />
         </div>
     </Navbar>
