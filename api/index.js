0b5ee8a66a6bb3868c72f01b75a4054ac248a7cf
Vamshi Gugulothu <vamnaik021@gmail.com>
2024-07-31 15:34:20 +0530

    Add OAuth functionality

--- api/index.js
+++ api/index.js
@@ -18,21 +18,22 @@ mongoose
 
 const app = express()
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('server is running on port 3000!')
 })
 
-app.use(express.json());
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 
 
-app.use((err,req,res,next) => {
-    const statuscode = err.statuscode || 500;
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
-    res.status(statuscode).json({
-        success: false,
-        statuscode: statuscode,
-        message: message,
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
     });
-});
+  });
